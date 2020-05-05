8b95881f325f56c07c1da963014fdc213a9366b3
lotofsoccer <59411035+lotofsoccer@users.noreply.github.com>
2020-05-05 16:15:45 +0700

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 from flask import Flask
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 @app.route('/')
 def home ():
+
+    html = requests.get(
+        "https://tuyensinh.ctu.edu.vn/chuong-trinh-dai-tra/841-danh-muc-nganh-va-chi-tieu-tuyen-sinh-dhcq.html").text
+
+    soup = BeautifulSoup(html, 'html5lib')
+
+    new_feed = soup.find('section', class_='article-content clearfix').find_all('a')
+    ND = soup.find('section', class_='article-content clearfix').find('p')
+    divs_TieuDe = soup("html", "com_content view-article itemid-285 j36 mm-hover")
+    divs_DL = soup("p", "MsoNormal")
+
+    DL = soup.find('section', class_='article-content clearfix').find_all('p')
+
+    TieuDe = divs_TieuDe[0].find("title").text
+    TieuDe1 = divs_TieuDe[0].find("h4").text
+    DuLieu = divs_DL[7].text
+
+    Ma_CN = divs_DL[6].text
+    Ten_CN = divs_DL[7].text
+    ToHop_CN = divs_DL[8].text
+    ChiTieu_CN = divs_DL[9].text
+    TT19_CN = divs_DL[10].text
+    TT18_CN = divs_DL[11].text
+    TT17_CN = divs_DL[12].text
+    print(Ma_CN, Ten_CN, ToHop_CN,
+    ChiTieu_CN,
+    TT19_CN,
+    TT18_CN,
+    TT17_CN)
     return '<h1> Deploy to Heroku  </h1>'
 
 if __name__ == '__main__' :
