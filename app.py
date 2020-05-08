a6d872044d9fa80ac56b612fd904243bf8bcc0e0
lotofsoccer <59411035+lotofsoccer@users.noreply.github.com>
2020-05-08 16:48:43 +0700

    make it better

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from bs4 import BeautifulSoup
 from flask import Flask
 import  requests
+import psycopg2
 
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
 @app.route('/')
 def index():
+    crawle()
+    connect()
+    return 'I am a live'
+
+def crawle ():
     html = requests.get("https://tuyensinh.ctu.edu.vn/chuong-trinh-dai-tra/841-danh-muc-nganh-va-chi-tieu-tuyen-sinh-dhcq.html").text
-    soup = BeautifulSoup(html, 'html5lib')
+    soup = BeautifulSoup(html, "lxml")
 
     new_feed = soup.find('section', class_='article-content clearfix').find_all('a')
     ND = soup.find('section', class_='article-content clearfix').find('p')
@@ -33,7 +39,32 @@ def index():
           TT19_CN,
           TT18_CN,
           TT17_CN)
-    return 'I am a live'
+
+def connect() :
+    try:
+        connection = psycopg2.connect(user="eecggdupfmuiyf",
+                                      password="9e43ebc95ea8c477df591eabeb26a42bfa7b77386ed2581c6acb6b74354e2176",
+                                      host="ec2-54-86-170-8.compute-1.amazonaws.com",
+                                      port="5432",
+                                      database="d3lhnjrp51u6dl")
+
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print(connection.get_dsn_parameters(), "\n")
+
+        # Print PostgreSQL version
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        print("You are connected to - ", record, "\n")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
 if __name__ == '__main__' :
 
